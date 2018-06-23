e1230037c4b0d77edda9536ce8542db3ef482df4
shantanubhattarai <shantanu.bhattarai271@gmail.com>
2018-06-23 17:17:49 +0545

    fixed table order

--- ludifyDB.sql
+++ ludifyDB.sql
@@ -16,6 +16,58 @@
 CREATE DATABASE IF NOT EXISTS `ludifydb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `ludifydb`;
 
+
+-- Dumping structure for table ludifydb.user_roles
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table ludifydb.user_roles: ~1 rows (approximately)
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` (`role_id`, `role_name`) VALUES
+	(1, 'user');
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+
+-- Dumping structure for table ludifydb.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` text,
+  `username` varchar(25) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table ludifydb.users: ~1 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`user_id`, `email`, `password`, `username`, `role_id`) VALUES
+	(1, 'example@example.com', 'example123', 'example_user', 1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+-- Dumping structure for table ludifydb.files
+CREATE TABLE IF NOT EXISTS `files` (
+  `file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `link` text NOT NULL,
+  `no_of_downloads` int(11) DEFAULT NULL,
+  PRIMARY KEY (`file_id`,`link`(100))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table ludifydb.files: ~2 rows (approximately)
+/*!40000 ALTER TABLE `files` DISABLE KEYS */;
+INSERT INTO `files` (`file_id`, `link`, `no_of_downloads`) VALUES
+	(1, '../files/06.23.18/Dumpper v.91.2.rar', 0),
+	(2, '../files/06.23.18/Capture.PNG', 0);
+/*!40000 ALTER TABLE `files` ENABLE KEYS */;
 -- Dumping structure for table ludifydb.articles
 CREATE TABLE IF NOT EXISTS `articles` (
   `article_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -39,54 +91,4 @@ INSERT INTO `articles` (`article_id`, `article_title`, `article_body`, `file_id`
 	(1005, 'Lorem', 'Lorem Ipsum dolor dolor I see a little fellow yello yello. heloo world.', 2, 1, 'category2', '2018-06-20');
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 
--- Dumping structure for table ludifydb.files
-CREATE TABLE IF NOT EXISTS `files` (
-  `file_id` int(11) NOT NULL AUTO_INCREMENT,
-  `link` text NOT NULL,
-  `no_of_downloads` int(11) DEFAULT NULL,
-  PRIMARY KEY (`file_id`,`link`(100))
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table ludifydb.files: ~2 rows (approximately)
-/*!40000 ALTER TABLE `files` DISABLE KEYS */;
-INSERT INTO `files` (`file_id`, `link`, `no_of_downloads`) VALUES
-	(1, '../files/06.23.18/Dumpper v.91.2.rar', 0),
-	(2, '../files/06.23.18/Capture.PNG', 0);
-/*!40000 ALTER TABLE `files` ENABLE KEYS */;
-
--- Dumping structure for table ludifydb.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `password` text,
-  `username` varchar(25) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `username` (`username`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table ludifydb.users: ~1 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`user_id`, `email`, `password`, `username`, `role_id`) VALUES
-	(1, 'example@example.com', 'example123', 'example_user', 1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
--- Dumping structure for table ludifydb.user_roles
-CREATE TABLE IF NOT EXISTS `user_roles` (
-  `role_id` int(11) NOT NULL,
-  `role_name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table ludifydb.user_roles: ~1 rows (approximately)
-/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
-INSERT INTO `user_roles` (`role_id`, `role_name`) VALUES
-	(1, 'user');
-/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
